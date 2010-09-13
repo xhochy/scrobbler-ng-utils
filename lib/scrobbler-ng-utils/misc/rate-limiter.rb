b71b416e424a39b7650cc3fd496fd83600da3f7c
Uwe L. Korn <uwelk@xhochy.com>
2010-09-13 17:10:26 +0200

    Increased rate.

--- lib/scrobbler-ng-utils/misc/rate-limiter.rb
+++ lib/scrobbler-ng-utils/misc/rate-limiter.rb
@@ -2,10 +2,13 @@ module Scrobbler
   class Base
     @@fetch_http_lastcall = 0
     class << self
+
+      NUMBER_OF_REQUESTS_PER_SECOND = 3
+
       alias :orig_fetch_http :fetch_http
       def fetch_http(request_method, paramlist)
         now = ("%10.6f" % Time.now.to_f).to_f
-        threshold = now - 1
+        threshold = now - (1.0/NUMBER_OF_REQUESTS_PER_SECOND)
         if (@@fetch_http_lastcall > threshold) then
           sleep(now - @@fetch_http_lastcall)
         end
